7a92a0d82602c746453c205d526bbeb4e5fae2df
unknown <barbambu123@gmail.com>
2022-05-14 19:49:05 -0600

    parte 3 del lab, se ve feo y cambios a la parte 1, porque tenia problemas

--- lab-1/lab.py
+++ lab-1/lab.py
@@ -1,29 +1,101 @@
 
 def parse_equation(equation):
-    result = equation.replace('+', '')
-    d = dict(x.split('x') for x in result.split())
+    result = equation.replace('+ ', '')
+    result = result.replace('- ', '-')
 
-    d = {f"x{v}": k for k, v in d.items()}
+    d = {}
+    for word in result.split():
+        key = word[-2:]
+        value = word[:-2]
+        d[key] = value
 
     for key, value in d.items():
         if value == '-':
             d[key] = -1
         if value == '':
             d[key] = 1
-
     return d
 
 
 def parse_restriction(restriction):
-
     upper_bound = False
-
-    equation = restriction.split('<=' or '>=')
-    d = parse_equation(equation[0])
-
-    if(restriction.find('<')):
+    if('<' in restriction):
+        equation = restriction.split('<=')
         upper_bound = True
+    else:
+        equation = restriction.split('>=')
 
+    d = parse_equation(equation[0])
+    
     equation[1] = equation[1].replace(' ', '')
-
+    
     return(d, upper_bound, equation[1])
+
+
+def parse_problem(objective, restrictions, maximize):
+    list_obj = []
+    list_var = []
+    list_rest = []
+
+    equation = parse_equation(objective)
+    for key, value in equation.items():
+        list_obj.append(value)
+        list_var.append(key)
+    
+    n_variables = len(list_var)
+
+    for x in restrictions:
+        restriction_dict = parse_restriction(x)
+        restriction = []
+        for i in range(n_variables):
+            val = restriction_dict[0].get('x'+ str(i+1))
+            if (val != None):
+                restriction.append(val)
+            else:
+                restriction.append(0)
+            if(i == n_variables-1):
+                restriction.append(restriction_dict[2])
+
+        list_rest.append(restriction)
+    n_restrictions = len(list_rest)
+
+    iteration = 1
+    for x in restrictions:
+        restriction_dict = parse_restriction(x)
+        
+        if(restriction_dict[1] == True):
+            list_var.append('s' + str(iteration))
+            list_obj.append(0)
+            i = 0
+            while i < n_restrictions:
+                if(i == iteration-1):
+                    list_rest[i].insert(len(list_rest[i])-1, 1)
+                else:
+                    list_rest[i].insert(len(list_rest[i])-1, 0)
+                i += 1
+
+        else:
+            list_var.append('s' + str(iteration))
+            list_var.append('a' + str(iteration))
+            list_obj.append(0)
+            if(maximize == True):
+                list_obj.append(-1000000000000)
+            else:
+                list_obj.append(1000000000000)
+            i = 0
+            while i < n_restrictions:
+                if(i == iteration-1):
+                    list_rest[i].insert(len(list_rest[i])-1, -1)
+                    list_rest[i].insert(len(list_rest[i])-1, 1)
+                else:
+                    list_rest[i].insert(len(list_rest[i])-1, 0)
+                    list_rest[i].insert(len(list_rest[i])-1, 0)
+                i += 1
+
+        if(iteration == n_restrictions):
+            for y in restrictions:
+                restriction_dict = parse_restriction(y)
+
+        iteration += 1
+
+    return(list_obj, list_var, list_rest)
--- lab-1/main.py
+++ lab-1/main.py
@@ -2,5 +2,14 @@ from lab import *
 
 if __name__ == "__main__":
 
-    equation = "-3.8x1 + x2 -2x3 <= 35"
-    print(parse_restriction(equation))
+    equation = "x1 -x2 <= 40"
+    #parse_restriction(equation)
+
+    restrictions_list = ["x1 + x2 <= 7", 
+                         "4x1 + 10x2 <= 40", 
+                         "10x1 >= 30"]
+    result = parse_problem("30x1 + 100x2", restrictions_list, True)
+
+    print(result[0])
+    print(result[1])
+    print(result[2])
