65a1da083b4c8f3e2bb3d8b0550ee37802fe8ad5
r-contreras <rcontreras1905@gmail.com>
2022-07-04 12:02:23 -0600

    avance tiempos e integracion con clases

--- lab-3/lab.py
+++ lab-3/lab.py
@@ -30,7 +30,7 @@ class Client():
 
     def end_service(self, time):
         service_end = time
-        self.service_time = service_end - self.service.start
+        self.service_time = service_end - self.service_start
         self.overall_time = self.queue_time + self.service_time
 
     def get_times(self):
@@ -44,15 +44,19 @@ class Server():
         self.idle = True
         self.idle_time = 0.0
         self.last_service = 0.0
+        self.current_client = None
         #Tiene que estar en SEGUNDOS
     
     def serve(self, client: Client, time):
         self.idle_time += time - self.last_service
         self.idle = False
+        self.current_client = client
         client.end_queue(time)
         client.start_service(time)
-        time += self.service_time #Revisar como se calcula cuanto dura cada server
-        client.end_service(time)
+
+    def end_serve(self, time):
+        self.current_client.end_service(time)
+        self.current_client = None
         self.last_service = time
         self.idle = True
     
@@ -61,6 +65,9 @@ class Server():
     
     def get_idle_time(self):
         return self.idle_time
+
+    def get_client(self):
+        return self.current_client
 
 class Queue():
 
@@ -71,16 +78,15 @@ class Queue():
         self.server_count = s
         self.lambd = lambd
         self.mu = mu
+        self.total_idle_time = 0.0
     
     def simulation(self, time_limit):
         n = 0
         current_time = 0
         queue = []
         servers = [Server() for i in range(self.server_count)]
-        print(servers)
-        return
         time_client = current_time + random_exp(self.lambd(n))  # Se calcula la llegada del primer cliente
-        new_event = (time_client, "New Client")
+        new_event = (time_client, Client())
         event_queue = []
         event_queue.append(new_event)  #se añade a la cola de eventos la hora del primer cliente
         
@@ -88,43 +94,58 @@ class Queue():
             current_event = event_queue[0]
             event_queue.pop(0)
 
-            if(current_event[1] == "New Client"):  # Si la cola de eventos saca un cliente nuevo
-                n = n + 1
+            if type(current_event[1]) is Client:  # Si la cola de eventos saca un cliente nuevo
+                if(n < self.lmax):
+                    n = n + 1
+                    self.client_count += 1
+                    
+
                 current_time = current_event[0]
 
-                index = np.where(servers == 1) # Se revisa si hay servidores disponibles
-                if len(index[0]) > 0: # Si hay servidores disponibles, entonces se pasa el cliente a un servidor y se calcula el tiempo de finalizacion del servicio
-                    servers[index[0][0]] = 0
-                
+                idle_servers = [idx for idx, s in enumerate(servers) if s.is_idle() == True]# Se revisa si hay servidores disponibles
+                if len(idle_servers) > 0: # Si hay servidores disponibles, entonces se pasa el cliente a un servidor y se calcula el tiempo de finalizacion del servicio
+                    index = idle_servers[0]
+                    server = servers[index]
+                    server.serve(current_event[1], current_time)
                     time_release_server = current_time + random_exp(self.mu(n))
-                    new_event = (time_release_server, servers[index[0][0]], "Server")
+                    new_event = (time_release_server, server)
                     event_queue.append(new_event)
 
                 else:  # Si no hay servidores disponibles, entonces se agrega el cliente a la cola.
-                    if(len(queue) < self.lmax):
+                    if len(queue) + self.server_count < self.lmax and current_time <= time_limit:
                         queue.append(current_event)
+                        client = current_event[1]
+                        client.start_queue(current_time)
+                    else:
+                        self.lost_count += 1
 
-                time_client = current_time + random_exp(self.lambd(n))
-                new_event = (time_client, "New Client")
-                event_queue.append(new_event)
+                if(current_time <= time_limit):
+                    time_client = current_time + random_exp(self.lambd(n))
+                    new_event = (time_client, Client())
+                    event_queue.append(new_event)
 
 
             else:  # Caso donde la cola de eventos sea que se liberó un servidor
                 n = n - 1
                 current_time = current_event[0]
+                server = current_event[1]
+                server.end_serve(current_time)
                 if len(queue) > 0:
-                    index = current_event[1]
                     current_event = queue[0]
                     queue.pop(0)
-
+                    client = current_event[1]
+                    server.serve(client, current_time)
                     time_release_server = current_time + random_exp(self.mu(n))
-                    new_event = (time_release_server, index, "Server")
+                    new_event = (time_release_server, server)
                     event_queue.append(new_event)
-
-                else:
-                    servers[current_event[1]] = 1
 
             event_queue.sort()
 
+        #Calcular tiempo sin hacer nada de los servidores
+        for s in servers:
+            self.total_idle_time += s.get_idle_time()
+        self.total_idle_time /= self.server_count
+
+        print(self.lost_count)
 
                 
