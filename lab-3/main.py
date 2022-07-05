b7bfce9e18354a58943f739c34092e7b302f3bc3
r-contreras <rcontreras1905@gmail.com>
2022-07-04 18:46:27 -0600

    elimina un print en main

--- lab-3/main.py
+++ lab-3/main.py
@@ -2,8 +2,6 @@ from lab import *
 import numpy as np
 
 if __name__ == "__main__":
-    print(random_exp(1/15))
-    print('\n')
     queue = Queue(15, 3, lambda n: 64 - n**(1.5), lambda n: 5 + 3*n)
     queue.simulation(1000)
     print('\n')
