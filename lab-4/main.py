dbf2e5abe5a26264eb7dcab7ee90c6f908fd75b1
unknown <barbambu123@gmail.com>
2022-07-12 21:18:12 -0600

    main

--- lab-4/main.py
+++ lab-4/main.py
@@ -13,10 +13,21 @@ if __name__ == "__main__":
 
     # c)
     # i.
-    card = Card(13, "SPADES")
-    card2 = Card(13, "CLUBS")
+    card = Card(1, "SPADES")
+    card2 = Card(1, "CLUBS")
     wins, loses, draws = simulate([card,card2], 100000, generator)
     print(wins, loses, draws)
     # ii.
-
+    values = good_abm(10000001)
+    generator = CongruentialGenerator(values[0], values[1], values[2])
+    card = Card(2, "SPADES")
+    card2 = Card(2, "CLUBS")
+    wins, loses, draws = simulate([card,card2], 100000, generator)
+    print(wins, loses, draws)
     # iii.
+    values = good_abm(10000001)
+    generator = CongruentialGenerator(values[0], values[1], values[2])
+    card = Card(2, "SPADES")
+    card2 = Card(7, "CLUBS")
+    wins, loses, draws = simulate([card,card2], 100000, generator)
+    print(wins, loses, draws)
