edff0197a5fbeca03ce2107fe1e3f0a78fb2a471
r-contreras <rcontreras1905@gmail.com>
2022-07-19 10:13:09 -0600

    Inicio proyecto

---
+++ proyecto/main.py
@@ -0,0 +1,5 @@
+from quarto import *
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
---
+++ proyecto/quarto.py
@@ -0,0 +1,21 @@
+import time, math, numpy, random
+
+class Quarto:
+    def __init__(self):
+        pass
+    def turn_player() -> bool:
+        return True
+    def turn_ai() -> bool:
+        return True
+    def has_finished() -> bool:
+        return True
+    def get_winner() -> int:
+        return 0
+    def get_available_actions() -> list[Action]:
+        return [None]
+    def do_action(action: Action):
+        return self
+
+class Action:
+    def __init__(self):
+        pass
